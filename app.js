50a338bccc7375f6928d97291492e9525ae52d53
Matt <matt@skynamo.com>
2022-03-25 16:34:59 +0200

    Add unkown option, attempt smart option generation

--- app.js
+++ app.js
@@ -20,75 +20,76 @@ const app = new App({
 var avaidMotd = [];
 var avaidSotd = [];
 var avaidFd = [];
+var avaidUknown = [];
 var claimedMeals = [];
 var hasSeenMessageToday = false;
 
 // MARK: Actions
-app.action("donate_motd", ({ event, say, body }) => {
-    avaidMotd.push({
-        owner: body.user.id,
-        ownerName: `<${body.user.id}>`,
-        type: "MOTD",
-        timeIn: event.time,
-        timeOut: ""
-    });
-    console.log(avaidMotd);
-});
+// app.action("donate_motd", ({ event, say, body }) => {
+//     avaidMotd.push({
+//         owner: body.user.id,
+//         ownerName: `<${body.user.id}>`,
+//         type: "MOTD",
+//         timeIn: event.time,
+//         timeOut: ""
+//     });
+//     console.log(avaidMotd);
+// });
 
-app.action("donate_sotd", ({ event, say, body }) => {
-    avaidSotd.push({
-        owner: body.user.id,
-        ownerName: `<${body.user.id}>`,
-        type: "SOTD",
-        timeIn: event.time,
-        timeOut: ""
-    });
-    console.log(avaidSotd);
-});
+// app.action("donate_sotd", ({ event, say, body }) => {
+//     avaidSotd.push({
+//         owner: body.user.id,
+//         ownerName: `<${body.user.id}>`,
+//         type: "SOTD",
+//         timeIn: event.time,
+//         timeOut: ""
+//     });
+//     console.log(avaidSotd);
+// });
 
-app.action("donate_fd", ({ event, say, body }) => {
-    avaidFd.push({
-        owner: body.user.id,
-        ownerName: `<${body.user.id}>`,
-        type: "FD",
-        timeIn: event.time,
-        timeOut: ""
-    });
-    console.log(avaidFd);
-});
+// app.action("donate_fd", ({ event, say, body }) => {
+//     avaidFd.push({
+//         owner: body.user.id,
+//         ownerName: `<${body.user.id}>`,
+//         type: "FD",
+//         timeIn: event.time,
+//         timeOut: ""
+//     });
+//     console.log(avaidFd);
+// });
 
-app.action("claim_sotd", async({ event, say, body }) => {
-    if (avaidSotd.length > 0) {
-        var meal = avaidSotd.shift();
-        meal["claimer"] = body.user.id;
-        meal["claimerName"] = `<${body.user.id}>`,
-        meal["timeOut"] = event.time;
-        await say("You got a SOTD from " + `<@${meal.owner}>`);
-        claimedMeals.push(meal);
-    }
-});
+// app.action("claim_sotd", async({ event, say, body }) => {
+//     if (avaidSotd.length > 0) {
+//         var meal = avaidSotd.shift();
+//         meal["claimer"] = body.user.id;
+//         meal["claimerName"] = `<${body.user.id}>`,
+//         meal["timeOut"] = event.time;
+//         await say("You got a SOTD from " + `<@${meal.owner}>`);
+//         claimedMeals.push(meal);
+//     }
+// });
 
-app.action("claim_motd", async({ event, say, body }) => {
-    if (avaidMotd.length > 0) {
-        var meal = avaidMotd.shift();
-        meal["claimer"] = body.user.id;
-        meal["claimerName"] = `<${body.user.id}>`,
-        meal["timeOut"] = event.time;
-        await say("You got a MOTD from " + `<@${meal.owner}>`);
-        claimedMeals.push(meal);
-    }
-});
+// app.action("claim_motd", async({ event, say, body }) => {
+//     if (avaidMotd.length > 0) {
+//         var meal = avaidMotd.shift();
+//         meal["claimer"] = body.user.id;
+//         meal["claimerName"] = `<${body.user.id}>`,
+//         meal["timeOut"] = event.time;
+//         await say("You got a MOTD from " + `<@${meal.owner}>`);
+//         claimedMeals.push(meal);
+//     }
+// });
 
-app.action("claim_fd", async({ event, say, body }) => {
-    if (avaidFd.length > 0) {
-        var meal = avaidFd.shift();
-        meal["claimer"] = body.user.id;
-        meal["claimerName"] = `<${body.user.id}>`,
-        meal["timeOut"] = event.time;
-        await say("You got a FD from " + `<@${meal.owner}>`);
-        claimedMeals.push(meal);
-    }
-});
+// app.action("claim_fd", async({ event, say, body }) => {
+//     if (avaidFd.length > 0) {
+//         var meal = avaidFd.shift();
+//         meal["claimer"] = body.user.id;
+//         meal["claimerName"] = `<${body.user.id}>`,
+//         meal["timeOut"] = event.time;
+//         await say("You got a FD from " + `<@${meal.owner}>`);
+//         claimedMeals.push(meal);
+//     }
+// });
 
 app.action("actionFeed", async({ body, ack, say }) => {
     await ack();
@@ -130,6 +131,14 @@ app.action("actionFeed", async({ body, ack, say }) => {
                         },
                         value: "value-fd",
                     },
+                    {
+                      text: {
+                          type: "plain_text",
+                          text: "Don't remember",
+                          emoji: true,
+                      },
+                      value: "value-unknown",
+                  },
                 ],
                 action_id: "meal-type-selected-give",
             },
@@ -141,7 +150,7 @@ app.action("actionFeed", async({ body, ack, say }) => {
 app.action("actionEat", async({ body, ack, say }) => {
     await ack();
     // await say(`<@${body.user.id}> you lazy fuck`);
-    await say("There are " + avaidMotd.length + " MOTD's, " + avaidSotd.length + " SOTD's and " + avaidFd.length + " FD's");
+    await say("There are " + avaidMotd.length + " MOTD's, " + avaidSotd.length + " SOTD's, " + avaidFd.length + " FD's and " + avaidUknown.length + "Lucky Packets");
     await say({
         blocks: [{
             type: "section",
@@ -156,45 +165,68 @@ app.action("actionEat", async({ body, ack, say }) => {
                     text: "Options",
                     emoji: true,
                 },
-                options: [{
-                        text: {
-                            type: "plain_text",
-                            text: "MOTD",
-                            emoji: true,
-                        },
-                        value: "value-motd",
-                    },
-                    {
-                        text: {
-                            type: "plain_text",
-                            text: "SOTD",
-                            emoji: true,
-                        },
-                        value: "value-sotd",
-                    },
-                    {
-                        text: {
-                            type: "plain_text",
-                            text: "FD",
-                            emoji: true,
-                        },
-                        value: "value-fd",
-                    },
-                    {
-                        text: {
-                            type: "plain_text",
-                            text: "I'll take anything",
-                            emoji: true,
-                        },
-                        value: "value-any",
-                    },
-                ],
+                availableOptionGenerator,
                 action_id: "meal-type-selected-take",
             },
         }, ],
         text: "home",
     });
 });
+
+function availableOptionGenerator() {
+  var fd = avaidFd.length;
+  var motd = avaidMotd.length;
+  var sotd = avaidSotd.length;
+  var unknown = avaidUknown.length;
+  var anyAvailable = fd > 0 | motd > 0 | sotd > 0 | unknown > 0;
+
+  var options = [];
+
+  if (motd > 0) {
+    options.push({
+      text: createOption("MOTD"),
+      value: "value-motd"
+    })
+  }
+  if (sotd > 0) {
+    options.push({
+      text: createOption("SOTD"),
+      value: "value-sotd"
+    })
+  }
+  if (fd > 0) {
+    options.push({
+      text: createOption("FD"),
+      value: "value-fd"
+    })
+  }
+  if (unknown > 0) {
+    options.push({
+      text: createOption("Lucky Packet"),
+      value: "value-unkown"
+    })
+  }
+  if (anyAvailable > 0) {
+    options.push({
+      text: createOption("I'll take anything"),
+      value: "value-any"
+    })
+  }
+  var final = ({
+    options: options
+  })
+
+  return JSON.stringify(final, null, 2);
+}
+
+function createOption(title) {
+  var option = ({
+    type: "plain_text",
+    text: title,
+    emoji: true,
+  });
+  return option;
+}
 
 app.action("meal-type-selected-take", async({ body, ack, say, action }) => {
     await ack();
@@ -207,6 +239,8 @@ app.action("meal-type-selected-take", async({ body, ack, say, action }) => {
         claimSOTD(say, body, action);
     } else if (value === 'value-fd') {
         claimFD(say, body, action);
+    } else if (value === 'value-unknown') {
+        claimUknown(say, body, action);
     } else {
         claimANY(say, body, action);
     }
@@ -223,6 +257,8 @@ app.action("meal-type-selected-give", async({ body, ack, say, action }) => {
         giveSOTD(say, body, action);
     } else if (value === 'value-fd') {
         giveFD(say, body);
+    } else if (value === 'value-uknown') {
+      giveUnkown(say, body, action);
     }
 });
 
@@ -268,6 +304,8 @@ function claimMOTD(say, body, action) {
         meal["timeOut"] = action.action_ts;
         say("You got a MOTD from " + `<@${meal.owner}>`);
         claimedMeals.push(meal);
+    } else {
+      say("No MOTD's available right now");
     }
 }
 
@@ -279,6 +317,8 @@ function claimSOTD(say, body, action) {
         meal["timeOut"] = action.action_ts;
         say("You got a SOTD from " + `<@${meal.owner}>`);
         claimedMeals.push(meal);
+    } else {
+      say("No SOTD's available right now");
     }
 }
 
@@ -290,18 +330,35 @@ function claimFD(say, body, action) {
         meal["timeOut"] = action.action_ts;
         say("You got a FD from " + `<@${meal.owner}>`);
         claimedMeals.push(meal);
+    } else {
+      say("No FD's available right now");
     }
+}
+
+function claimUknown(say, body, action) {
+  if (avaidUknown.length > 0) {
+      var meal = avaidFd.shift();
+      meal["claimer"] = body.user.id;
+      meal["claimerName"] = `<@${body.user.id}>`,
+      meal["timeOut"] = action.action_ts;
+      say("You got a Lucky Packet from " + `<@${meal.owner}>`);
+      claimedMeals.push(meal);
+  } else {
+    say("No Lucky Packets available right now");
+  }
 }
 
 function claimANY(say, body, action) {
     var fd = avaidFd.length;
     var motd = avaidMotd.length;
     var sotd = avaidSotd.length;
+    var unkown = avaidUknown.length;
 
     var available = [];
     if (fd > 0) available.push('fd');
     if (motd > 0) available.push('motd');
     if (sotd > 0) available.push('sotd');
+    if (unkown > 0) available.push('unkown');
 
     if (available.length > 0) {
         var choice = Math.floor(Math.random()) * available.length - 1;
@@ -311,7 +368,9 @@ function claimANY(say, body, action) {
             claimSOTD(say, body, action);
         } else if (available[choice] === 'fd') {
             claimFD(say, body, action);
-        }
+        } else if (available[choice] === 'unkown') {
+          claimUknown(say, body, action);
+      }
     } else {
         say("Oh no! No meals available right now, try again later ");
     }
--- empty.js
+++ empty.js
@@ -37,4 +37,23 @@ meal["timeOut"] = new Date();
 claimedMeals.push(meal);
 
 var c = JSON.stringify(claimedMeals, null, 2)
-console.log(c);
+// console.log(c);
+
+var option = [];
+
+var text = ({
+		  type: "plain_text",
+		  text: "MOTD",
+		  emoji: true,
+	  });
+
+option.push({
+	text: text,
+	value: "abc"
+})
+var total = ({
+	options: option
+})
+
+var d = JSON.stringify(total, null, 2);
+console.log(d);
