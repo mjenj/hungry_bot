b4602c6cc08d939421f6c8aec42dd4638fb9c4a4
LouisBotha <louis@skynamo.com>
2022-03-25 11:16:59 +0200

    it wil listen on port now

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+var http = require('http');
+http.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('Hello World!');
+    res.end();
+}).listen(process.env.PORT || 3000);
+
 // Require the Bolt package (github.com/slackapi/bolt)
 const { App } = require("@slack/bolt");
 
